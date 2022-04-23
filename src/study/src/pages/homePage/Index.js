c6737f794e68e4f145176ba6666495ddf1b1b6a4
huynhvanthuy97 <huynhvanthuy97@gmail.com>
2022-04-23 16:15:40 +0700

    Create sample menus

--- src/study/src/pages/homePage/Index.js
+++ src/study/src/pages/homePage/Index.js
@@ -9,6 +9,8 @@ import {
   VideoCameraOutlined,
   UploadOutlined,
 } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import SubMenu from "antd/lib/menu/SubMenu";
 
 const { Header, Sider, Content } = Layout;
 
@@ -19,34 +21,63 @@ const Home = () => {
     setCollapsed(!collapsed);
   };
 
+  const menus = [
+    {
+      key: "1",
+      icon: <UserOutlined />,
+      label: "Dashboard",
+      link: "/",
+    },
+    {
+      key: "2",
+      icon: <VideoCameraOutlined />,
+      label: "Products",
+      link: "/",
+    },
+    {
+      key: "3",
+      icon: <UploadOutlined />,
+      label: "Categories",
+      link: "/",
+    },
+  ];
+
   return (
     <Layout>
-      <Sider
-       trigger={null} collapsible collapsed={collapsed}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo" />
-        <Menu
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={[
-            {
-              key: "1",
-              icon: <UserOutlined />,
-              label: "nav 1",
-            },
-            {
-              key: "2",
-              icon: <VideoCameraOutlined />,
-              label: "nav 2",
-            },
-            {
-              key: "3",
-              icon: <UploadOutlined />,
-              label: "nav 3",
-            },
-          ]}
-        />
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          {menus.map((item, key) => (
+            <Menu.Item key={item.key}>
+              {item.icon}
+              <span>{item.label}</span>
+              <Link to={item.icon} />
+            </Menu.Item>
+          ))}
+          <SubMenu
+            key="sub4"
+            title={
+              <span>
+                <UserOutlined />
+                <span>Sub menu 1</span>
+              </span>
+            }
+          >
+            <Menu.Item>Chill menu 1</Menu.Item>
+            <SubMenu
+              title={
+                <span>
+                  <span>Chill menu 2</span>
+                </span>
+              }
+            >
+              <Menu.Item>Chill menu 2 - 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item>Chill menu 3</Menu.Item>
+            <Menu.Item>Chill menu 4</Menu.Item>
+          </SubMenu>
+        </Menu>
       </Sider>
-
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
           {collapsed ? (
