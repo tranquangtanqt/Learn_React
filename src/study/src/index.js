2bf2c30e0a63be57aafc65c1e46a59f307939d13
tranquangtanqt <tranquangtanqt1990@gmail.com>
2022-04-23 14:26:10 +0700

    Add route

--- src/study/src/index.js
+++ src/study/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/homePage/Index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <h1>123123</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+     </BrowserRouter>
   </React.StrictMode>
 );
