1805dec9d61ccd510f021c26636d3cd513a6e6e7
AUREOLE\tantq <tantq@aureole-it.vn>
2022-04-25 09:12:25 +0700

    edit menu

--- src/study/src/components/modules/code/index.js
+++ src/study/src/components/modules/code/index.js
@@ -1,24 +1,8 @@
-import React, { useEffect } from "react";
-import Prism from "prismjs";
-//import "prismjs/themes/prism-tomorrow.css";
-//import "./styles.css";
-
+import React from "react";
 import Editor from "react-prism-editor";
 
 export default function Code({ code, language }) {
-//   useEffect(() => {
-//     setTimeout(() => {
-//         Prism.highlightAll();
-//     }, 0);
-//   }, []);
 
-//   return (
-//     <div className="Code">
-//       <pre>
-//         <code className={`language-${language}`}>{code}</code>
-//       </pre>
-//     </div>
-//   );
 return (
     <Editor
       language={language}
@@ -30,7 +14,6 @@ return (
       showLanguage={true}
       changeCode={(code) => {
         this.code = code;
-        console.log(code);
       }}
     />
   );
--- src/study/src/themes/masterLayout/index.js
+++ src/study/src/themes/masterLayout/index.js
@@ -6,10 +6,10 @@ import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   UserOutlined,
-  VideoCameraOutlined,
-  UploadOutlined,
+  // VideoCameraOutlined,
+  // UploadOutlined,
 } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import SubMenu from "antd/lib/menu/SubMenu";
 
 const { Header, Sider, Content } = Layout;
@@ -21,43 +21,62 @@ const MasterLayout = ({ children }) => {
     setCollapsed(!collapsed);
   };
 
-  const menus = [
-    {
-      key: "1",
-      icon: <UserOutlined />,
-      label: "Dashboard",
-      link: "/",
-    },
-    {
-      key: "2",
-      icon: <VideoCameraOutlined />,
-      label: "About",
-      link: "/about",
-    },
-    {
-      key: "3",
-      icon: <UploadOutlined />,
-      label: "HighLightCode",
-      link: "/highlightcode",
-    },
-  ];
+  // const menus = [
+  //   {
+  //     key: "1",
+  //     icon: <UserOutlined />,
+  //     label: "Dashboard",
+  //     link: "/",
+  //   },
+  //   {
+  //     key: "2",
+  //     icon: <VideoCameraOutlined />,
+  //     label: "About",
+  //     link: "/about",
+  //   },
+  //   {
+  //     key: "3",
+  //     icon: <UploadOutlined />,
+  //     label: "HighLightCode",
+  //     link: "/highlightcode",
+  //   }
+  // ];
 
   return (
     <Layout>
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo" />
         <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-          {menus.map((item) => (
+          {/* {menus.map((item) => (
             <Menu.Item key={item.key}>
               {item.icon}
               <span>{item.label}</span>
               <Link to={item.link} />
             </Menu.Item>
-          ))}
+          ))} */}
           {
-            <SubMenu
-              key="sub4"
-              title={
+            <SubMenu key="sub4" title={
+                <span>
+                  <UserOutlined />
+                  <span>Utilities</span>
+                </span>
+              }
+            >
+              <Menu.Item>Chill menu 1</Menu.Item>
+              <SubMenu title={
+                  <span>
+                    <span>Chill menu 2</span>
+                  </span>
+                }
+              >
+                <Menu.Item>Chill menu 2 - 2</Menu.Item>
+              </SubMenu>
+              <Menu.Item>Chill menu 3</Menu.Item>
+              <Menu.Item>Chill menu 4</Menu.Item>
+            </SubMenu>
+          }
+          {
+            <SubMenu key="demo" title={
                 <span>
                   <UserOutlined />
                   <span>Sub menu 1</span>
@@ -65,8 +84,7 @@ const MasterLayout = ({ children }) => {
               }
             >
               <Menu.Item>Chill menu 1</Menu.Item>
-              <SubMenu
-                title={
+              <SubMenu title={
                   <span>
                     <span>Chill menu 2</span>
                   </span>
@@ -89,7 +107,7 @@ const MasterLayout = ({ children }) => {
           )}
         </Header>
 
-        <Content className="site-layout-background">{children}</Content>
+        <Content className="site-layout-background-content">{children}</Content>
       </Layout>
     </Layout>
   );
