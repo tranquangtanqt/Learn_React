9415f3499c9e5e52f51bfbd3c1902f21eab7e4ec
AUREOLE\tantq <tantq@aureole-it.vn>
2022-05-05 10:56:58 +0700

    edit repeat text

--- src/study/src/pages/utilities/repeatText/index.js
+++ src/study/src/pages/utilities/repeatText/index.js
@@ -1,25 +1,83 @@
-import { Tabs } from "antd";
+import { Button, Col, Divider, Input, Row } from "antd";
+import { useState } from "react";
 import MasterLayout from "../../../themes/masterLayout";
-import RepeatTextMulti from "./repeatTextMulti";
-import RepeatTextOne from "./repeatTextOne";
 
 const RepeatText = () => {
-  const callback = (key) => {
-    console.log(key);
+  
+  const [input, setInput] = useState("###replace###");
+  const [output, setOutput] = useState("");
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+
+  const renderText = () => {
+    let outputStr = "";
+    if (isNaN(start)) {
+      let startNumber = start.charCodeAt(0);
+      let endNumber = end.charCodeAt(0);
+      for (let i = startNumber; i <= endNumber; i++) {
+        let str = replaceAll(input, "###replace###", String.fromCharCode(i));
+
+        outputStr += str;
+        outputStr += "\r\n";
+      }
+    } else {
+      // number
+      for (let i = parseInt(start); i <= parseInt(end); i++) {
+        let str = replaceAll(input, "###replace###", i);
+        outputStr += str;
+        outputStr += "\r\n";
+      }
+    }
+
+    setOutput(outputStr);
   };
 
-  const { TabPane } = Tabs;
+  const replaceAll = (string, search, replace) => {
+    return string.split(search).join(replace);
+  };
 
   return (
     <MasterLayout>
-      <Tabs defaultActiveKey="2" onChange={callback}>
-        <TabPane tab="ONE" key="1">
-          <RepeatTextOne></RepeatTextOne>
-        </TabPane>
-        <TabPane tab="MULTI" key="2">
-        <RepeatTextMulti></RepeatTextMulti>
-        </TabPane>
-      </Tabs>
+       <Divider orientation="left">Input</Divider>
+        <Row justify="start">
+          <Col span={24}>
+            <textarea
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              style={{ height: 200, width: "100%" }}
+            />
+          </Col>
+        </Row>
+
+        <Divider orientation="left">Start</Divider>
+        <Input
+          placeholder="Start"
+          value={start}
+          onChange={(e) => setStart(e.target.value)}
+        />
+
+        <Divider orientation="left">End</Divider>
+        <Input
+          placeholder="End"
+          value={end}
+          onChange={(e) => setEnd(e.target.value)}
+        />
+
+        <Divider orientation="left">
+          <Button type="primary" onClick={renderText}>
+            Render text
+          </Button>
+        </Divider>
+
+        <Row justify="start">
+          <Col span={24}>
+            <textarea
+              value={output}
+              onChange={(e) => setOutput(e.target.value)}
+              style={{ height: 200, width: "100%" }}
+            />
+          </Col>
+        </Row>
     </MasterLayout>
   );
 };
