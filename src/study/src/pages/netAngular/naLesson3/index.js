072e10b6a32ce8301d6ec4d63ae35ec1885ba147
AUREOLE\tantq <tantq@aureole-it.vn>
2022-05-11 19:15:38 +0700

    lesson 3

--- src/study/src/pages/netAngular/naLesson3/index.js
+++ src/study/src/pages/netAngular/naLesson3/index.js
@@ -1,16 +1,9 @@
-import { Col, Row, Divider, Image } from "antd";
+import { Col, Row, Divider } from "antd";
 import Code from "../../../components/modules/code";
 import MasterLayout from "../../../themes/masterLayout";
+import { naLesson3Data } from './data';
 
 const NALesson3 = () => {
-const user = `namespace API.Entities
-{
-    public class AppUser
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-    }
-}`;
 
   return (
     <MasterLayout>
@@ -22,30 +15,26 @@ const user = `namespace API.Entities
         <Col span={23} offset={1}>
           <p>- Create folder Entities</p>
           <p>- Create class AppUser</p>
-          <Code code={user} language="csharp" />
+          <Code code={naLesson3Data.user} language="csharp" />
         </Col>
 
         <Col span={24}>
-          <h1>1. Adding a DBContext class</h1>
+          <h1>2. Adding a DBContext class</h1>
         </Col>
         <Col span={23} offset={1}>
-          <p>- An Object Relational Mapper (ORM)</p>
-          <p>
-            - Translates our code into SQL commands that update our tables in
-            the database
-          </p>
-          <Image
-            src={require("../../../resources/img/netAngular/Net_Angular_4.jpg")}
-          />
+          <p>- Add folder: API\Data</p>
+          <p>- Add class: DataContext</p>
+          <Code code={naLesson3Data.dataContex} language="csharp" />
         </Col>
+
         <Col span={24}>
-          <h1>2. Entity Framework Features</h1>
+          <h1>3. Creating the connection string</h1>
         </Col>
         <Col span={23} offset={1}>
-          <p>
-            - Querying: EF allows us to query our database using link queries
-            that I mentioned very briefly before
-          </p>
+          <p>- Editing appsettings.Development.json</p>
+          <Code code={naLesson3Data.appsettings} language="json" />
+          <p>- Startup.cs</p>
+          <Code code={naLesson3Data.startUp} language="csharp" />
         </Col>
       </Row>
     </MasterLayout>
