6e3d8ab2cc748ed7f3cc2aa51f5cba2c0882528d
AUREOLE\tantq <tantq@aureole-it.vn>
2022-06-22 11:39:07 +0700

    edit tnsname

--- src/study/src/pages/database/oracle/listenerAndTnsnames/data.js
+++ src/study/src/pages/database/oracle/listenerAndTnsnames/data.js
@@ -41,4 +41,54 @@ ORCL =
         )
     )
   `,
+  listenerRemote: `SID_LIST_LISTENER =
+  (SID_LIST =
+    (SID_DESC =
+      (SID_NAME = CLRExtProc)
+      (ORACLE_HOME = D:\\app\\tantq\\virtual\\product\\12.2.0\\dbhome_1)
+      (PROGRAM = extproc)
+      (ENVS = "EXTPROC_DLLS=ONLY:D:\\app\\tantq\\virtual\\product\\12.2.0\\dbhome_1\\bin\\oraclr12.dll")
+    )
+    (SID_DESC =
+      (GLOBAL_DBNAME = ORCL)
+      (ORACLE_HOME = D:\\app\\tantq\\virtual\\product\\12.2.0\\dbhome_1)
+      (SID_NAME = ORCL)
+    )
+  )
+
+LISTENER =
+  (DESCRIPTION_LIST =
+    (DESCRIPTION =
+      (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1521))
+    )
+    (DESCRIPTION =
+      (ADDRESS = (PROTOCOL = TCP)(HOST = 0.0.0.0)(PORT = 1521))
+    )
+  )
+
+ADR_BASE_LISTENER = D:\\app\\tantq\\virtual\\product\\12.2.0\\dbhome_1\\log
+`,
+  tnsnamesRemote: `LISTENER_ORCL =
+  (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.145.101)(PORT = 1521))
+
+ORACLR_CONNECTION_DATA =
+  (DESCRIPTION =
+    (ADDRESS_LIST =
+      (ADDRESS = (PROTOCOL = IPC)(KEY = EXTPROC1521))
+    )
+    (CONNECT_DATA =
+      (SID = CLRExtProc)
+      (PRESENTATION = RO)
+    )
+  )
+
+ORCL =
+  (DESCRIPTION =
+    (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.145.101)(PORT = 1521))
+    (CONNECT_DATA =
+      (SERVER = DEDICATED)
+      (SERVICE_NAME = ORCL)
+    )
+  )`,
+
 }
--- src/study/src/pages/database/oracle/listenerAndTnsnames/index.js
+++ src/study/src/pages/database/oracle/listenerAndTnsnames/index.js
@@ -11,10 +11,41 @@ const ListenerAndTnsnames = () => {
       <h1>Database Oracle</h1>
       <Row justify="start">
         <Col span={24}>
-          <h1>1. listener.ora</h1>
+          <h1>1. listener.ora original</h1>
+        </Col>
+
+        <Col span={23} offset={1}>
           <Code code={ListenerAndTnsnamesData.listener} language="javascript" />
-          <h1>2. tnsnames.ora</h1>
+        </Col>
+      </Row>
+
+      <Row justify="start">
+        <Col span={24}>
+          <h1>2. tnsnames.ora original</h1>
+        </Col>
+
+        <Col span={23} offset={1}>
           <Code code={ListenerAndTnsnamesData.tnsnames} language="javascript" />
+        </Col>
+      </Row>
+
+      <Row justify="start">
+        <Col span={24}>
+          <h1>3. listener.ora remote</h1>
+        </Col>
+
+        <Col span={23} offset={1}>
+          <Code code={ListenerAndTnsnamesData.listenerRemote} language="javascript" />
+        </Col>
+      </Row>
+
+      <Row justify="start">
+        <Col span={24}>
+          <h1>4. tnsnames.ora remote</h1>
+        </Col>
+
+        <Col span={23} offset={1}>
+          <Code code={ListenerAndTnsnamesData.tnsnamesRemote} language="javascript" />
         </Col>
       </Row>
     </MasterLayout>
