91d0dc17a0e58340a9f0739158a86a7db8975bba
AUREOLE\tantq <tantq@aureole-it.vn>
2022-07-18 18:49:56 +0700

    oracle

--- src/study/src/pages/database/oracle/tips/system.js
+++ src/study/src/pages/database/oracle/tips/system.js
@@ -10,7 +10,26 @@ const SystemOracle = () => {
     <MasterLayout>
       <Row justify="start">
         <Col span={24}>
-          <h1>1. Thư mục lưu file tablespace</h1>
+          <h1>1. Hiển thị toàn bộ comments của table và view</h1>
+        </Col>
+        <Col span={23} offset={1}>
+          <Code code={`SELECT * FROM all_tab_comments WHERE comments LIKE '%%';`} language="sql" />
+        </Col>
+      </Row>
+
+      <Row justify="start">
+        <Col span={24}>
+          <h1>2. Hiển thị toàn bộ comments của column</h1>
+        </Col>
+        <Col span={23} offset={1}>
+          <Code code={`SELECT * FROM all_col_comments WHERE table_name LIKE '%%' OR comments LIKE '%%'`} language="sql" />
+        </Col>
+      </Row>
+
+
+      <Row justify="start">
+        <Col span={24}>
+          <h1>3. Thư mục lưu file tablespace</h1>
         </Col>
         <Col span={23} offset={1}>
           <Code code={`D:\\app\\tantq\\virtual\\product\\12.2.0\\dbhome_1\\database`} language="markup" />
@@ -19,7 +38,7 @@ const SystemOracle = () => {
 
       <Row justify="start">
         <Col span={24}>
-          <h1>2. Kiểm tra user đang sử dụng</h1>
+          <h1>4. Kiểm tra user đang sử dụng</h1>
         </Col>
         <Col span={23} offset={1}>
           <Code code={`SELECT user FROM dual`} language="sql" />
@@ -28,7 +47,7 @@ const SystemOracle = () => {
 
       <Row justify="start">
         <Col span={24}>
-          <h1>3. Kiểm tra dung lượng DB</h1>
+          <h1>5. Kiểm tra dung lượng DB</h1>
         </Col>
         <Col span={23} offset={1}>
           <Code code={`SELECT SUM(bytes)/1024/1024/1024  FROM user_segments\nSELECT SUM(bytes)/1024/1024 FROM user_segments where segment_name='FCM_RESOURCE'`} language="sql" />
@@ -37,7 +56,7 @@ const SystemOracle = () => {
 
       <Row justify="start">
         <Col span={24}>
-          <h1>4. Hiển thị tất cả các cột</h1>
+          <h1>6. Hiển thị tất cả các cột</h1>
         </Col>
         <Col span={23} offset={1}>
           <Code code={`SELECT table_name, column_name, data_type, data_length FROM USER_TAB_COLUMNS`} language="sql" />
